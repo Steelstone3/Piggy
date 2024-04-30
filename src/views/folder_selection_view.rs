787e64bcbc4fe2a8b35741688337a75a982a5cbf
Skippy <steelstone3@gmail.com>
2024-04-30 04:15:14 +0100

    update

--- src/views/folder_selection_view.rs
+++ src/views/folder_selection_view.rs
@@ -1,42 +1,3 @@
-// use crate::{commands::messages::Message, models::dive_step::DiveStep};
-// use iced::widget::{column, text, text_input, Column, Text, TextInput};
-
-// pub struct DiveStepView<'a> {
-//     dive_step_text: Text<'a>,
-//     depth_text: Text<'a>,
-//     depth_input: TextInput<'a, Message>,
-//     time_text: Text<'a>,
-//     time_input: TextInput<'a, Message>,
-// }
-
-// impl DiveStepView<'_> {
-//     pub fn build_view<'a>(dive_step: &DiveStep) -> Column<'a, Message> {
-//         let dive_step = DiveStepView::new(dive_step);
-
-//         column![
-//             dive_step.dive_step_text,
-//             dive_step.depth_text,
-//             dive_step.depth_input,
-//             dive_step.time_text,
-//             dive_step.time_input,
-//         ]
-//         .spacing(10.0)
-//         .padding(10.0)
-//     }
-
-//     fn new(dive_step: &DiveStep) -> Self {
-//         Self {
-//             dive_step_text: text("Dive Step"),
-//             depth_text: text("Depth (m)"),
-//             depth_input: text_input("Enter Depth", &dive_step.depth.to_string())
-//                 .on_input(Message::DepthChanged),
-//             time_text: text("Time (min)"),
-//             time_input: text_input("Enter Time", &dive_step.time.to_string())
-//                 .on_input(Message::TimeChanged),
-//         }
-//     }
-// }
-
 use crate::{commands::piggy_message::PiggyMessage, models::piggy_settings::PiggySettings};
 use iced::widget::{column, text, text_input, Column, Text, TextInput};
 
@@ -59,7 +20,7 @@ impl FolderSelectionView<'_> {
 
     fn new<'a>(piggy_settings: &PiggySettings) -> FolderSelectionView<'a> {
         FolderSelectionView {
-            folder_selection_text: text("Folder:"),
+            folder_selection_text: text("Project Folder:"),
             folder_selection_input: text_input("Enter Folder", &piggy_settings.folder)
                 .on_input(PiggyMessage::FolderChanged),
         }
