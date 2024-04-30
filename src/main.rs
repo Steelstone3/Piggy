f216619834da08e353f0b2303c0b63d7f89e6a73
Skippy <steelstone3@gmail.com>
2024-04-30 17:25:57 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::{Sandbox, Settings};
+use iced::{window, Sandbox, Settings, Size};
 use models::piggy::Piggy;
 
 mod commands;
@@ -6,5 +6,14 @@ mod models;
 mod views;
 
 pub fn main() -> iced::Result {
-    Piggy::run(Settings::default())
+    Piggy::run(Settings {
+        window: window::Settings {
+            min_size: Some(Size {
+                width: 640.0,
+                height: 480.0,
+            }),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
 }
