8587076761780f1266155764931d8ddaffbf1afd
Skippy <steelstone3@gmail.com>
2024-05-02 00:52:25 +0100

    Update FileFinderShould.cs

--- PiggyTests/Controllers/FileFinderShould.cs
+++ PiggyTests/Controllers/FileFinderShould.cs
@@ -4,32 +4,10 @@ using Xunit;
 public class FileFinderShould
 {
     [Fact]
-    public void FindFileUseDefaultFoundFile()
-    {
-        // Given
-        string filePath = "example.json";
-        string expectedFilePath = "example.json";
-        Mock<IPrompt> prompt = new();
-        prompt.Setup(p => p.UseDefault()).Returns(true);
-        Mock<IFile> file = new();
-        file.Setup(f => f.IsExistingFile(filePath)).Returns(true);
-        IFileFinder fileFinder = new FileFinder(prompt.Object);
-
-        // When
-        string actualFilePath = fileFinder.FindFile(file.Object, filePath);
-
-        // Then
-        prompt.VerifyAll();
-        file.VerifyAll();
-        Assert.Equal(expectedFilePath, actualFilePath);
-    }
-
-    [Fact]
     public void FindFileUseDefaultFileNotFound()
     {
         // Given
         string filePath = "example.json";
-        string expectedFilePath = "example.json";
         Mock<IPrompt> prompt = new();
         prompt.Setup(p => p.UseDefault()).Returns(true);
         Mock<IFile> file = new();
@@ -43,6 +21,47 @@ public class FileFinderShould
         // Then
         prompt.VerifyAll();
         file.VerifyAll();
-        Assert.Equal(expectedFilePath, actualFilePath);
+        Assert.Equal(filePath, actualFilePath);
+    }
+
+    [Fact]
+    public void FindFileUseDefaultFoundFile()
+    {
+        // Given
+        string filePath = "example.json";
+        Mock<IPrompt> prompt = new();
+        prompt.Setup(p => p.UseDefault()).Returns(true);
+        Mock<IFile> file = new();
+        file.Setup(f => f.IsExistingFile(filePath)).Returns(true);
+        IFileFinder fileFinder = new FileFinder(prompt.Object);
+
+        // When
+        string actualFilePath = fileFinder.FindFile(file.Object, filePath);
+
+        // Then
+        prompt.VerifyAll();
+        file.VerifyAll();
+        Assert.Equal(filePath, actualFilePath);
+    }
+
+
+     [Fact]
+    public void FindFileNotUsingDefault()
+    {
+        // Given
+        string filePath = "example.json";
+        Mock<IFile> file = new();
+        Mock<IPrompt> prompt = new();
+        prompt.Setup(p => p.UseDefault()).Returns(false);
+        prompt.Setup(p => p.FindFile(file.Object)).Returns(filePath);
+        IFileFinder fileFinder = new FileFinder(prompt.Object);
+
+        // When
+        string actualFilePath = fileFinder.FindFile(file.Object, filePath);
+
+        // Then
+        prompt.VerifyAll();
+        file.VerifyAll();
+        Assert.Equal(filePath, actualFilePath);
     }
 }
