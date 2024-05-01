c97fb62b7d42bc94dcdf817bcb10c2d1c43a026d
Skippy <steelstone3@gmail.com>
2024-05-02 00:48:26 +0100

    Update FileFinderShould.cs

--- PiggyTests/Controllers/FileFinderShould.cs
+++ PiggyTests/Controllers/FileFinderShould.cs
@@ -1,14 +1,48 @@
+using Moq;
 using Xunit;
 
 public class FileFinderShould
 {
-    [Fact(Skip ="Later")]
-    public void TestName()
+    [Fact]
+    public void FindFileUseDefaultFoundFile()
     {
         // Given
-    
+        string filePath = "example.json";
+        string expectedFilePath = "example.json";
+        Mock<IPrompt> prompt = new();
+        prompt.Setup(p => p.UseDefault()).Returns(true);
+        Mock<IFile> file = new();
+        file.Setup(f => f.IsExistingFile(filePath)).Returns(true);
+        IFileFinder fileFinder = new FileFinder(prompt.Object);
+
         // When
-    
+        string actualFilePath = fileFinder.FindFile(file.Object, filePath);
+
         // Then
+        prompt.VerifyAll();
+        file.VerifyAll();
+        Assert.Equal(expectedFilePath, actualFilePath);
+    }
+
+    [Fact]
+    public void FindFileUseDefaultFileNotFound()
+    {
+        // Given
+        string filePath = "example.json";
+        string expectedFilePath = "example.json";
+        Mock<IPrompt> prompt = new();
+        prompt.Setup(p => p.UseDefault()).Returns(true);
+        Mock<IFile> file = new();
+        file.Setup(f => f.IsExistingFile(filePath)).Returns(false);
+        file.Setup(f => f.CreateDefaultPiggyConfigurationFile());
+        IFileFinder fileFinder = new FileFinder(prompt.Object);
+
+        // When
+        string actualFilePath = fileFinder.FindFile(file.Object, filePath);
+
+        // Then
+        prompt.VerifyAll();
+        file.VerifyAll();
+        Assert.Equal(expectedFilePath, actualFilePath);
     }
 }
