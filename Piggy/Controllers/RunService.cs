cb8c1f703d02c0b408f478c29cb8ff4d29f7e7e9
Skippy <steelstone3@gmail.com>
2024-05-02 01:26:03 +0100

    update

--- Piggy/Controllers/RunService.cs
+++ Piggy/Controllers/RunService.cs
@@ -1,9 +1,22 @@
 public class RunService : IRunService
 {
-    // TODO move program into here
+    public void Run()
+    {
+        string filePath = "piggy.json";
+        IPrompt prompt = new Prompt();
+        IFile file = new File();
+        IFileFinder fileFinder = new FileFinder(prompt);
+
+        IJobExecutor jobExecutor = new JobExecutor();
+        IRunner runService = new Runner(prompt, jobExecutor);
+
+        filePath = fileFinder.FindFile(file, filePath);
+
+        runService.Run(file, filePath);
+    }
 }
 
-public interface IRunService 
+public interface IRunService
 {
-
+    void Run();
 }
--- Piggy/Program.cs
+++ Piggy/Program.cs
@@ -2,16 +2,7 @@ internal static class Program
 {
     internal static void Main()
     {
-        string filePath = "piggy.json";
-        IPrompt prompt = new Prompt();
-        IFile file = new File();
-        IFileFinder fileFinder = new FileFinder(prompt);
-        
-        IJobExecutor jobExecutor = new JobExecutor();
-        IRunner runService = new Runner(prompt, jobExecutor);
-
-        filePath = fileFinder.FindFile(file, filePath);
-
-        runService.Run(file, filePath);
+       IRunService runService = new RunService();
+       runService.Run();
     }
 }
