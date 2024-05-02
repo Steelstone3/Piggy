653836ea618fcb2e49763e961cf04bf3485d797c
Skippy <steelstone3@gmail.com>
2024-05-02 01:20:42 +0100

    Update RunnerShould.cs

--- PiggyTests/Controllers/RunnerShould.cs
+++ PiggyTests/Controllers/RunnerShould.cs
@@ -5,7 +5,7 @@ using Xunit;
 public class RunnerShould
 {
     [Fact]
-    public void RunOneCommand()
+    public void RunOneJobCommand()
     {
         // Given
         string commandOutput = "example";
@@ -34,5 +34,29 @@ public class RunnerShould
         file.VerifyAll();
         prompt.VerifyAll();
         jobExecutor.VerifyAll();
+    }
+
+    [Fact]
+    public void DoNotRunJob()
+    {
+        // Given
+        string filePath = "piggy.json";
+        Job job = new();
+        Piggy piggy = new();
+        Mock<IFile> file = new();
+        file.Setup(f => f.ReadPiggyConfigurationFile(filePath)).Returns(piggy);
+        Mock<IJobExecutor> jobExecutor = new();
+        Mock<IPrompt> prompt = new();
+        prompt.Setup(p => p.JobSelection(piggy.Jobs)).Returns(job);
+        prompt.Setup(p => p.ConfirmSelectedJob(job)).Returns(false);
+        prompt.Setup(p => p.ConfirmContinue()).Returns(false);
+        IRunner runner = new Runner(prompt.Object, jobExecutor.Object);
+
+        // When
+        runner.Run(file.Object, filePath);
+
+        // Then
+        file.VerifyAll();
+        prompt.VerifyAll();
     }
 }
