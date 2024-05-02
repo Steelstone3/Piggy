a806938bcd7ea2dedba834d1cb31998ca401c0c7
Skippy <steelstone3@gmail.com>
2024-05-02 01:23:09 +0100

    Update Program.cs

--- Piggy/Program.cs
+++ Piggy/Program.cs
@@ -2,15 +2,16 @@ internal static class Program
 {
     internal static void Main()
     {
-        IPrompt prompt = new Prompt();
-        IFileFinder fileFinder = new FileFinder(prompt);
-
         string filePath = "piggy.json";
+        IPrompt prompt = new Prompt();
         IFile file = new File();
-        filePath = fileFinder.FindFile(file, filePath);
-
+        IFileFinder fileFinder = new FileFinder(prompt);
+        
         IJobExecutor jobExecutor = new JobExecutor();
         IRunner runService = new Runner(prompt, jobExecutor);
+
+        filePath = fileFinder.FindFile(file, filePath);
+
         runService.Run(file, filePath);
     }
 }
