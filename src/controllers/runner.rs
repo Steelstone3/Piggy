cdb3f1b768660e2117666c1c1ae3295f11ae964e
Alex Howson <alex.howson@kratos.co.uk>
2024-05-03 14:25:02 +0100

    Update runner.rs

--- src/controllers/runner.rs
+++ src/controllers/runner.rs
@@ -6,7 +6,11 @@ pub fn run(file_path: String) {
     let piggy = read_piggy_configuration_file(&file_path);
 
     while is_not_quit {
+        let complete_project_folder = format!("Project: {}\nConfiguration: {}", piggy.piggy_settings.project_folder_location, piggy.piggy_settings.configuration_file_location);
+        println!("{}", complete_project_folder);
+
         let job = job_selection(&piggy.jobs);
+
         if confirmation_yes(&job.expect("No Job Selected").display_job_details()) {
             execute(job.expect("No Job Selected"), &piggy.piggy_settings)
         } else {
